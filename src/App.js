5950bc7b54ad4e864bc67dd5ebed2ab075208d5b
Siddhesh Jain <jainsiddhesh777@gmail.com>
2022-02-09 14:37:44 +0530

    Initial commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,60 @@
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import axios from "axios";
+import Coincard from "./Card";
+import { Grid } from "@mui/material";
+import "./App.css";
+
+const url =
+  "https://api.coingecko.com/api/v3/coins/markets?vs_currency=INR&order=market_cap_desc&per_page=1000&page=1&sparkline=false";
 
 function App() {
+  const [coins, setCoins] = React.useState([]);
+  const [search, setSearch] = React.useState("");
+  const [visible ,setVisible] = React.useState(3);
+
+  axios.get(url).then((res) => {
+    setCoins(res.data);
+  });
+
+  function handleChange(e) {
+    setSearch(e.target.value);
+  }
+
+  function LoadMore()
+  {
+    setVisible(visible+3);
+  }
+
+  const filteredCoins = coins.filter((coin) =>
+    coin.name.toLowerCase().includes(search.toLowerCase())
+  );
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="container">
+      <div className="Navbar">
+        <p className="Branding">💲 C r y p t o - B a z a a r 💲</p>
+        <form>
+          <input
+            type="text"
+            placeholder="Search Coin"
+            onChange={handleChange}
+            className="coin-search"
+          />
+        </form>
+      </div>
+      <Grid
+        container
+        style={{ gap: 15 }}
+        justifyContent="center"
+        className="data"
+      >
+        {filteredCoins.slice(0,visible).map(Coincard)}
+      </Grid>
+      {visible<1000 &&
+      <div  variant="primary" onClick={LoadMore} className="AddButton" >
+        V i e w  M o r e
+      </div>}
+      <div className="footer">Copyright © 2022</div>
     </div>
   );
 }
-
 export default App;
---
+++ src/Card.js
@@ -0,0 +1,58 @@
+import React from "react";
+import {Card,CardContent,CardMedia,Typography,Grid} from "@mui/material";
+import "./App.css";
+import CountUp from "react-countup";
+
+export default function Coincard(coin) {
+
+  return (
+      <Grid item component={Card} spacing={3} className="card">
+        <CardMedia
+          component="img"
+          height="140"
+          image={coin.image}
+          alt="green iguana"
+          className="coinImage"
+        />
+        <CardContent>
+          <Typography gutterBottom variant="h5" component="div">
+            {coin.name} ({coin.symbol.toUpperCase()})
+          </Typography>
+          <Typography variant="body1" color="text.secondary">
+            Price : ₹ {coin.current_price}
+            {coin.price_change_percentage_24h < 0 ? (
+              <span className="red">
+                (⬇{coin.price_change_percentage_24h.toFixed(2)})%
+              </span>
+            ) : (
+              <span className="green">
+                (⬆{coin.price_change_percentage_24h.toFixed(2)}%)
+              </span>
+            )}
+          </Typography>
+          <Typography variant="body2" color="text.secondary" className="data">
+            Volume :
+            <CountUp
+              start={0}
+              end={coin.total_volume}
+              duration={2}
+              separator=","
+            />
+          </Typography>
+          <Typography variant="body1" color="text.secondary" className="data">
+            Market Cap :
+            <CountUp
+              start={0}
+              end={coin.market_cap}
+              duration={2}
+              separator=","
+            />
+          </Typography>
+        </CardContent>
+        {/* <CardActions>
+          <Button size="small">Share</Button>
+          <Button size="small">Learn More</Button>
+        </CardActions> */}
+      </Grid>
+  );
+}
